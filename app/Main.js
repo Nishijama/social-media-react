9c46fb82dfa524e076f95f5261cea2c8ccf79091
Nishijama <nathanplociennik@gmail.com>
2022-11-20 23:50:23 +0100

    split the compontents into separate files

---
+++ app/Main.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// MY COMPONENTS
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+
+function Main() {
+  return (
+    <>
+      <Header />
+      <HomeGuest />
+      <Footer />
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<Main />);
+
+if (module.hot) {
+  module.hot.accept();
+}
